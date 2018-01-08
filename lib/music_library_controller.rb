21a0205f4f6fd66f8c1bc6e7da4fcfe2e489135e
Rebecca Tong <rtong@buffalo.edu>
2018-01-08 19:11:49 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -11,7 +11,7 @@ class MusicLibraryController
     puts "To list all of the genres in your library, enter 'list genres'."
     puts "To list all of the songs by a particular artist, enter 'list artist'."
     puts "To list all of the songs of a particular genre, enter 'list genre'."
-    expect($stdout).to receive(:puts).with("To play a song, enter 'play song'."
+    puts "To play a song, enter 'play song'."
     expect($stdout).to receive(:puts).with("To quit, type 'exit'."
     expect($stdout).to receive(:puts).with("What would you like to do?"
   end
