4f5d105f8b5d59606fd516cc1a210e72644df18e
Rebecca Tong <rtong@buffalo.edu>
2018-01-08 19:53:36 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -29,6 +29,8 @@ class MusicLibraryController
      end
 
      def list_artists
+       Song.all.sort_by(&:name).each.with_index(1) do |song,index|
+         puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
      end
 
 end
