bc46d5fbeaf00ea00485556b57f538c400caa561
Rebecca Tong <rtong@buffalo.edu>
2018-01-08 18:33:51 +0000

    automatically backed up by learn

---
+++ lib/musicimporter.rb
@@ -0,0 +1,15 @@
+class MusicImporter
+
+attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    files = Dir.glob("#{path}/*.mp3")
+    files.collect do |song|
+      song.gsub("./spec/fixtures/mp3s/","")
+  end
+end
+end
