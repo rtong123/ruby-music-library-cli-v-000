04519e7444b6a3efe28f2c7cace4438d571c3ce3
Rebecca Tong <rtong@buffalo.edu>
2018-01-08 19:23:54 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -6,6 +6,7 @@ class MusicLibraryController
 
   def call
     input = ""
+    while input != "exit"
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
@@ -16,6 +17,8 @@ class MusicLibraryController
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
     input = gets.strip
+
+
   end
 
 
