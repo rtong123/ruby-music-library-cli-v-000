0d016141d80d98df23d3c6db0617488fe271c2e9
Rebecca Tong <rtong@buffalo.edu>
2018-01-08 20:52:56 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -67,6 +67,6 @@ class MusicLibraryController
        if (1..Song.all.length).include?[input]
        song = Song.all.sort_by(&:name)[input-1]
      end
-       puts "Playing #{song.name} by #{song.artist.name}" if not
+       puts "Playing #{song.name} by #{song.artist.name}" if song
    end
  end
