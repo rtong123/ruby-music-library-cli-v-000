2c605dc33ab2f57c5c7f8b88b5d807f6aef79d22
Rebecca Tong <rtong@buffalo.edu>
2018-01-08 21:16:21 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -17,11 +17,11 @@ class MusicLibraryController
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
     input = gets.strip
-
+end
     if input == "list songs"
       list_songs
     end
-    end
+  end
 
      def list_songs
         Song.all.sort_by(&:name).each.with_index(1) do |song, idx|
