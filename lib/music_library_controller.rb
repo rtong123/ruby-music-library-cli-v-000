a34d8561754365777bae75a5bb23fb2f40206ea5
Rebecca Tong <rtong@buffalo.edu>
2018-01-08 20:24:12 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -42,5 +42,6 @@ class MusicLibraryController
    def list_songs_by_artist
      puts "Please enter the name of an artist:"
      input = gets.strip
+     if artist = Artist.find_by_name(input)
    end
  end
