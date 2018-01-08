7c556e816864df31341a7ee697606ed0e03cd969
Rebecca Tong <rtong@buffalo.edu>
2018-01-08 19:05:07 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,7 +1,7 @@
 class MusicLibraryController
 
   def initialize(path=("./db/mp3s"))
-    MusicImporter.new(path= ("./spec/fixtures/mp3s"))
+    MusicImporter.new(path="./spec/fixtures/mp3s")
   end
 
 end
