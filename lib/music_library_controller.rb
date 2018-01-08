65808128bfd21371ec8ef390669ca74608a14b8b
Rebecca Tong <rtong@buffalo.edu>
2018-01-08 20:30:50 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -54,7 +54,7 @@ class MusicLibraryController
       puts "Please enter the name of a genre:"
       input = gets.strip
 
-       if artist = Artist.find_by_name(input)
+       if genre = Genre.find_by_name(input)
          artist.songs.sort_by(&:name).each.with_index(1) do |song, idx|
            puts "#{idx}. #{song.name} - #{song.genre.name}"
          end
