d4cb9d9c825057731a36a5745e7c6629cb8b52aa
Rebecca Tong <rtong@buffalo.edu>
2018-01-08 18:44:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -68,7 +68,7 @@ def self.find_or_create_by_name(name)
 end
 
 def self.new_from_filename(name)
-  artist,name,genre = name.split(" - ")
+  artist,song,genre = name.split(" - ")
   new_genre = genre.gsub(".mp3"," ")
   artist = Artist.find_or_create_by_name(artist)
   new_genre = Genre.find_or_create_by_name(genre)
