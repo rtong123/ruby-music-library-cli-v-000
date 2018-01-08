838d429c6706e4fc30a220b0ec5cda03e6cbb5d7
Rebecca Tong <rtong@buffalo.edu>
2018-01-08 21:15:50 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -17,6 +17,9 @@ class MusicLibraryController
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
     input = gets.strip
+
+    if input == "list songs"
+      list_songs
       end
     end
 
